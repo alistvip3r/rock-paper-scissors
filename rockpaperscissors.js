c2556e17b19a84a2787f90ca4d9e97c9c6d7de77
alistvip3r <251541935+alistvip3r@users.noreply.github.com>
2026-01-15 20:39:35 +0530

    Added function for human choice

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -16,3 +16,17 @@ function getComputerChoice() {
     }
 }
 console.log(getComputerChoice());
+
+function getHumanChoice() {
+    let choice = parseInt(prompt("Enter 0 for rock, 1 for paper or 2 for scissors respectively"));
+    if (choice === 0){
+        return r;
+    }
+    else if (choice === 1) {
+        return s;
+    }
+    else {
+        return p;
+    }
+}
+console.log(getHumanChoice());
