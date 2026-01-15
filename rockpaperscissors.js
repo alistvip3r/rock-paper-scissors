41e4f2b65d7097f32b3c1f3072d9c1b607d773ca
alistvip3r <251541935+alistvip3r@users.noreply.github.com>
2026-01-15 21:33:06 +0530

    fix: To accept user input as string

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -20,15 +20,19 @@ function getComputerChoice() {
 console.log(getComputerChoice());
 
 function getHumanChoice() {
-    let choice = parseInt(prompt("Enter 0 for rock, 1 for paper or 2 for scissors respectively"));
-    if (choice === 0){
+    const input = prompt("Type rock, paper or scissors");
+    const choice = String(input.toLowerCase());
+    if (choice === 'rock'){
         return rock;
     }
-    else if (choice === 1) {
+    else if (choice === 'scissors') {
         return scissors;
     }
-    else {
+    else if (choice === 'paper'){
         return paper;
+    }
+    else {
+        alert('Invalid input');
     }
 }
 console.log(getHumanChoice());
