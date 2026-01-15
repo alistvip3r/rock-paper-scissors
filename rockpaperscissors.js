53cfb87bdf8159204707bb05fdd81424f976b843
alistvip3r <251541935+alistvip3r@users.noreply.github.com>
2026-01-15 21:01:32 +0530

    Changed variables names

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,18 +1,20 @@
 let randomChoice = Math.floor(Math.random() * 3);
 console.log(randomChoice);
-const r = "rock";
-const p = "paper";
-const s = "scissors";
+const rock = "Rock";
+const paper = "Paper";
+const scissors = "Scissors";
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
     if (randomChoice === 0) { 
-        return r;
+        return rock;
     }
     else if (randomChoice === 1) {
-        return s;
+        return scissors;
     }
     else {
-        return p;
+        return paper;
     }
 }
 console.log(getComputerChoice());
@@ -20,13 +22,13 @@ console.log(getComputerChoice());
 function getHumanChoice() {
     let choice = parseInt(prompt("Enter 0 for rock, 1 for paper or 2 for scissors respectively"));
     if (choice === 0){
-        return r;
+        return rock;
     }
     else if (choice === 1) {
-        return s;
+        return scissors;
     }
     else {
-        return p;
+        return paper;
     }
 }
 console.log(getHumanChoice());
